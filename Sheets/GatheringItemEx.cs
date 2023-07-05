1374521b9c017e436cae0488492d5ddeb06825e2
Critical <criticalimpact@gmail.com>
2023-07-05 21:37:15 +1000

    Add missing nodes

--- Sheets/GatheringItemEx.cs
+++ Sheets/GatheringItemEx.cs
@@ -11,30 +11,31 @@ namespace CriticalCommonLib.Sheets
 {
     public class GatheringItemEx : GatheringItem
     {
+        //GatheringPointBase => GatheringItemId
         public static readonly Dictionary<uint,uint> Items = new()
         {
-            {203, 256},  // Grade 1 La Noscean Topsoil
-            // {7761, 200},  // Grade 1 Shroud Topsoil   
-            // {7764, 201},  // Grade 1 Thanalan Topsoil 
-            // {7759, 150},  // Grade 2 La Noscean Topsoil
-            // {7762, 209},  // Grade 2 Shroud Topsoil   
-            // {7765, 151},  // Grade 2 Thanalan Topsoil 
-            // {10092, 210}, // Black Limestone          
-            // {10094, 177}, // Little Worm              
-            // {10097, 133}, // Yafaemi Wildgrass        
-            // {12893, 295}, // Dark Chestnut            
-            // {15865, 30},  // Firelight Seeds          
-            // {15866, 39},  // Icelight Seeds           
-            // {15867, 21},  // Windlight Seeds          
-            // {15868, 31},  // Earthlight Seeds         
-            // {15869, 25},  // Levinlight Seeds         
-            // {15870, 14},  // Waterlight Seeds
-            // {12534, 285}, // Mythrite Ore             
-            // {12535, 353}, // Hardsilver Ore           
-            // {12537, 286}, // Titanium Ore             
-            // {12579, 356}, // Birch Log                
-            // {12878, 297}, // Cyclops Onion            
-            // {12879, 298}, // Emerald Beans            
+            {203, 256},  // Grade 1 La Noscean Topsoil, 7758
+            {200, 259},// Grade 1 Shroud Topsoil, 7761
+            {201, 262},  // Grade 1 Thanalan Topsoil 
+            {150, 257},  // Grade 2 La Noscean Topsoil
+            {209, 260},  // Grade 2 Shroud Topsoil   
+            {151, 263},  // Grade 2 Thanalan Topsoil 
+            {210, 289}, // Black Limestone          
+            {177, 293}, // Little Worm              
+            {133, 295}, // Yafaemi Wildgrass        
+            {295, 351}, // Dark Chestnut            
+            {30, 410},  // Firelight Seeds          
+            {39, 411},  // Icelight Seeds           
+            {21, 412},  // Windlight Seeds          
+            {31, 413},  // Earthlight Seeds         
+            {25, 414},  // Levinlight Seeds         
+            {14, 415},  // Waterlight Seeds
+            {285, 301}, // Mythrite Ore             
+            {353, 313}, // Hardsilver Ore           
+            {286, 307}, // Titanium Ore             
+            {356, 357}, // Birch Log                
+            {297, 347}, // Cyclops Onion            
+            {298, 352}, // Emerald Beans            
         };
 
         public override void PopulateData(RowParser parser, GameData gameData, Language language)
