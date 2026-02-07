e2cd51cbd228a68d6bc81a63e81222c258a8dc43
Critical Impact <criticalimpact@gmail.com>
2026-02-07 23:31:36 +1000

    Add extra WindowName

--- Services/Ui/WindowName.cs
+++ Services/Ui/WindowName.cs
@@ -5,44 +5,45 @@ namespace CriticalCommonLib.Services.Ui
         RetainerList,
         InventoryRetainer,//For normal retainer inventory
         RetainerGrid,//For normal retainer inventory
-            
+
         RetainerGrid0,//For expanded retainer inventory
         RetainerGrid1,//For expanded retainer inventory
         RetainerGrid2,//For expanded retainer inventory
         RetainerGrid3,//For expanded retainer inventory
         RetainerGrid4,//For expanded retainer inventory
         InventoryRetainerLarge,//For expanded retainer inventory
-            
+
         InventoryGrid0E,//For open all inventory
         InventoryGrid1E,//For open all inventory
         InventoryGrid2E,//For open all inventory
         InventoryGrid3E,//For open all inventory
         InventoryExpansion,//For open all inventory
-            
+
         InventoryBuddy, //Chocobo Saddlebag,
         InventoryBuddy2, //Chocobo Saddlebag but with a 2, why are they like this
-            
+
         Inventory, //For normal inventory
         InventoryGrid, //For normal inventory
         InventoryLarge, //For expanded inventory
         InventoryGrid0, //For expanded inventory
         InventoryGrid1, //For expanded inventory
         RetainerSellList,
-        
+
         ArmouryBoard,
         FreeCompanyChest,
         MiragePrismPrismBox,
+        MiragePrismMiragePlate,
         Cabinet,
         CabinetWithdraw,
-        
+
         Synthesis,
         SynthesisSimple,
         SubmarinePartsMenu,
-        
+
         HousingGoods,//Interior and Exterior placement window for housing items
         HousingEditExterior,
         HousingEditInterior,
-        
+
         FreeCompany,
         FreeCompanyCreditShop,
         SelectIconString,
